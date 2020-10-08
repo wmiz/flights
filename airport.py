890df2f6433f667ac8e99899439673bb3a295c97
wmisback <will.misback@gmail.com>
2020-10-08 19:31:13 -0400

    Add airport class

---
+++ airport.py
@@ -0,0 +1,23 @@
+class Airport:
+	# Holds flight data
+	def __init__(self, code):
+		self.code = code
+
+	def self.setCode(self, code):
+		self.code = code
+
+	def self.setName(self, name):
+		self.name = name
+
+	def self.setCountry(self, country):
+		self.country = country
+
+
+	def self.getCode(self):
+		return self.code
+
+	def self.getName(self):
+		return self.name
+
+	def self.getCountry(self):
+		return self.country
